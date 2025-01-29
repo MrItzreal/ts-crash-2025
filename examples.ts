eb98be3cd5d89328528d68f686e3c034dcf60e13
Israel Santana <is.wsie@gmail.com>
2025-01-29 11:54:45 -0500

    Learned:
    -How to move nested object types into a separate "type" definition.

--- examples.ts
+++ examples.ts
@@ -3,21 +3,24 @@ let myName: string = "Izzy";
 let numberOfWheels: number = 4;
 let isStudent: boolean = false;
 
-// DEFINING CUSTOM TYPES:
+// DEFINING CUSTOM TYPES
 // Example #1:
 type Food = string;
 let favoriteFood: Food = "pizza";
 
+// DEFINING CUSTOM TYPES
 // Example #2:
+type Address = {
+  street: string;
+  city: string;
+  country: string;
+};
+
 type Person = {
   name: string;
   age: number;
   isStudent: boolean;
-  address: {
-    street: string;
-    city: string;
-    country: string;
-  };
+  address: Address;
 };
 
 let person1: Person = {
@@ -41,7 +44,7 @@ let person2: Person = {
     country: "USA",
   },
 };
- 
+
 /* NOTES:
 -The "type" keyword creates a new name for a type. This exists only in TS and is great for readability and code reuse. By convention, the name that we give our type starts with a capital letter ex: type Food.
 */
